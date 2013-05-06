465956f3dce8a027a36c20e15f2941f49468fe0a
Adrian Weber <adrian.weber@cde.unibe.ch>
2013-05-06 09:15:01 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python
+
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Adrian Weber, Centre for Development and Environment, University of Bern"
+__date__ ="$May 6, 2013 9:03:55 AM$"
+
+if __name__ == "__main__":
+    print "Hello World";
